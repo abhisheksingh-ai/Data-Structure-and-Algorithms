73c4e8f4174000b2aea0e9ab790d4a8d4fa91a75
Abhishek Singh <abhisheksingh@kgpian.iitkgp.ac.in>
2024-06-13 09:51:56 +0530

    setPrecision with fixed

--- Algozenith/Phase0/day5/set_precision.cpp
+++ Algozenith/Phase0/day5/set_precision.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
+
 int main() {
     double number = 123.456789;
     
     // Set precision to 5 digits in total
-    cout << setprecision(5) << number << endl;  // Output might be in scientific notation for large numbers
-    //123.46
+    cout << setprecision(5) << number << endl; 
     return 0;
 }
+
+// Using Fixed with setprecision
+
+// Combining setprecision with fixed changes the behavior, so the precision strictly refers to the number of digits after the decimal point, and numbers are displayed in fixed-point notation.
+
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    double number = 123.456789;
+    
+    // Set precision to 2 digits after the decimal point, with fixed-point notation
+    cout << fixed << setprecision(4) << number << endl;  // Output: 123.4568
+
+    return 0;
+}
+
